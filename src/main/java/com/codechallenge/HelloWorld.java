428912354833582ee2cfe5169281b4489a1e45c9
Krishna Pandian <kpandian@ucsc.edu>
2021-06-25 13:56:44 -0700

    We tried

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -5,9 +5,70 @@ import java.util.ArrayList;
 /**
  * Created by merciersj on 8/29/2018.
  */
-public class HelloWorld {
+ 
 
+
+public class HelloWorld {
+	public static int[] Boo = new int[11];
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		//System.out.println("Hello World");
+		Boo[0] = 0;
+		Boo[1] = 1;
+		Boo[2] = 2;
+		Boo[3] = 3;
+		Boo[4] = 4;
+		Boo[5] = 5;
+		Boo[6] = 6;
+		Boo[7] = 7;
+		Boo[8] = 8;
+		Boo[9] = 9;
+		Boo[10] = 10;
+		int pYTHONiSbETTER = 0;
+		/*
+		 * Who uses java lmao
+		 *
+		 */
+		int JAVAAAAAAA = 1000000;
+		while (1 > (0 * 2)) {
+			
+			Boo[10] = (int)(hehe()); Boo[9] = (int)(hehe()); Boo[8] = (int)(hehe());Boo[7] = (int)(hehe());Boo[6] = (int)(hehe());Boo[5] = (int)(hehe());Boo[4] = (int)(hehe());Boo[3] = (int)(hehe());Boo[2] = (int)(hehe());Boo[1] = (int)(hehe());Boo[0] = (int)(hehe());
+			///System.out.println("stuck");
+			pYTHONiSbETTER++;
+			if (Boo[0] == (int)'h') {
+				if (Boo[1] == (int)'e') {
+					if (Boo[2] == (int)'l') {
+						if (Boo[3] == (int)'l') {
+							if (Boo[4] == (int)'o') {
+								if (Boo[5] == (int)' ') {
+									if (Boo[6] == (int)'w') {
+										if (Boo[7] == (int)'o') {
+											if (Boo[8] == (int)'r') {
+												if (Boo[9] == (int)'l') {
+													if (Boo[10] == (int)'d') {
+														System.out.println("Hello World");
+														return;
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			if (pYTHONiSbETTER == JAVAAAAAAA) {
+				break;
+			}
+		}
+		//System.out.println("oof");
+		System.out.print("Hello World");
 	}
+	public static int hehe() {
+		return (int)(Math.random() * (127 - 97) + (97)); 
+		//return (2 * 2 * 2 * 2 * 2 * 2 * 2);
+	}
+	
 }
