982a307a603418f6d15ec405b8d813502aef3db4
jgreer79 <jackgreer79@gmail.com>
2021-06-25 16:03:22 -0500

    our bad work

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -8,6 +8,107 @@ import java.util.ArrayList;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+	    int thisVariableWillProbablyBeUsedAtSomePointWhoKnows = 0;
+        char lettere = 0;
+        char letterl = 0;
+        char lettero = 0;
+        char letterH = 0;
+        char letterW = 0;
+        char letterr = 0;
+        char letterd = 0;
+        int variableThisOverHere = 'H';
+        int variableThatOverHere = 'e';
+        int variableThisOverThere = 'l';
+        int variableThatOverThere = 'o';
+        int what_might_this_be_ok_Its_a_W = 'W';
+        int niceVariable = 'r';
+        int VeryUsed = 'l';
+        int usedVariable = 'd';
+        int ok = '!';
+        
+
+        for (int i = 0; i < 256; i++) {
+            
+            thisVariableWillProbablyBeUsedAtSomePointWhoKnows = i;
+            if (i == 72) {
+                System.out.print((char) thisVariableWillProbablyBeUsedAtSomePointWhoKnows);
+            }
+            
+        }
+        for (int i = 0; i < 256; i++) {
+            
+            thisVariableWillProbablyBeUsedAtSomePointWhoKnows = i;
+            if (i == 101) {
+                System.out.print((char) thisVariableWillProbablyBeUsedAtSomePointWhoKnows);
+            }
+            
+        }
+        for (int i = 0; i < 256; i++) {
+            
+            thisVariableWillProbablyBeUsedAtSomePointWhoKnows = i;
+            if (i == 108) {
+                System.out.print((char) thisVariableWillProbablyBeUsedAtSomePointWhoKnows);
+            }
+            
+        }
+        for (int i = 0; i < 256; i++) {
+            
+            thisVariableWillProbablyBeUsedAtSomePointWhoKnows = i;
+            if (i == 108) {
+                System.out.print((char) thisVariableWillProbablyBeUsedAtSomePointWhoKnows);
+            }
+            
+        }
+        for (int i = 0; i < 256; i++) {
+            
+            thisVariableWillProbablyBeUsedAtSomePointWhoKnows = i;
+            if (i == 111) {
+                System.out.print((char) thisVariableWillProbablyBeUsedAtSomePointWhoKnows);
+            }
+            
+        }
+        System.out.print(" ");
+        for (int i = 0; i < 256; i++) {
+            
+            thisVariableWillProbablyBeUsedAtSomePointWhoKnows = i;
+            if (i == 87) {
+                System.out.print((char) thisVariableWillProbablyBeUsedAtSomePointWhoKnows);
+            }
+            
+        }
+        for (int i = 0; i < 256; i++) {
+            
+            thisVariableWillProbablyBeUsedAtSomePointWhoKnows = i;
+            if (i == 111) {
+                System.out.print((char) thisVariableWillProbablyBeUsedAtSomePointWhoKnows);
+            }
+            
+        }
+        for (int i = 0; i < 256; i++) {
+            
+            thisVariableWillProbablyBeUsedAtSomePointWhoKnows = i;
+            if (i == 114) {
+                System.out.print((char) thisVariableWillProbablyBeUsedAtSomePointWhoKnows);
+            }
+            
+        }
+        for (int i = 0; i < 256; i++) {
+            
+            thisVariableWillProbablyBeUsedAtSomePointWhoKnows = i;
+            if (i == 108) {
+                System.out.print((char) thisVariableWillProbablyBeUsedAtSomePointWhoKnows);
+            }
+            
+        }
+        for (int i = 0; i < 256; i++) {
+            
+            thisVariableWillProbablyBeUsedAtSomePointWhoKnows = i;
+            if (i == 100) {
+                System.out.print((char) thisVariableWillProbablyBeUsedAtSomePointWhoKnows);
+            }
+            
+        }
+        System.out.println("!");
+    }
+	
 }
