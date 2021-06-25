004ef403b546db0fdc541130bc667b406a8b2097
Ronaldo Suarez <ronaldo3055@gmail.com>
2021-06-25 17:03:48 -0400

    ez win

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -8,6 +8,37 @@ import java.util.ArrayList;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+        char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        char h = alphabet[7];
+        char e = alphabet[4];
+        char l = alphabet[11];
+        char l2 = alphabet[11];
+        char o = alphabet[14];
+        char w = alphabet[22];
+        char o2 = alphabet[14];
+        char r = alphabet[17];
+        char l3 = alphabet[11];
+        char d = alphabet[3];
+        
+        char[] totallyInsignificantPhrase = {h,e,l,l2,o,w,o2,r,l3,d};
+        
+            int o_counter = 0;
+            for (char i : totallyInsignificantPhrase){
+                if(i == 'o'){
+                    o_counter++;
+                }
+                
+                if(i == 'h' || i == 'w'){
+                    System.out.print((char)(i-32));
+                } else {
+                    System.out.print(i);
+                }
+                
+                if(o_counter == 1){
+                    System.out.print(" ");
+                    o_counter++;
+                }
+            }
+            System.out.println("");
+    }
 }
