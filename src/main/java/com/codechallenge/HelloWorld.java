78a2c2037e02191c387d1d5694deb6f951ecad12
Pixel-Yohan <yohanflores00@gmail.com>
2021-06-25 15:55:57 -0500

    our messy code

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -1,6 +1,10 @@
 package com.codechallenge;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.StringBuilder;
 
 /**
  * Created by merciersj on 8/29/2018.
@@ -8,6 +12,33 @@ import java.util.ArrayList;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		File file = new File("titanic_script.txt");
+		try {
+			StringBuilder x = new StringBuilder();
+			Scanner scanner = new Scanner(file);
+			while(scanner.hasNextLine()) {
+				x.append(scanner.nextLine());
+			}
+			int arr[] = new int[11];
+			arr[0]=x.lastIndexOf("H");
+			arr[1]=x.lastIndexOf("e");
+			arr[2]=x.lastIndexOf("l");
+			arr[3]=x.lastIndexOf("l");
+			arr[4]=x.lastIndexOf("o");
+			arr[5]=x.lastIndexOf(" ");
+			arr[6]=x.lastIndexOf("W");
+			arr[7]=x.lastIndexOf("o");
+			arr[8]=x.lastIndexOf("r");
+			arr[9]=x.lastIndexOf("l");
+			arr[10]=x.lastIndexOf("d");
+			for(int i=0; i<arr.length; i++){
+				System.out.print(x.charAt(arr[i]));
+			}
+
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		//System.out.println("Hello World");
 	}
 }
