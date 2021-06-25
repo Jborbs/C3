31e3bea9440aa535a97477af507e8bc07fae6cd8
Samuel Wallace Stegall <stegall@tamu.edu>
2021-06-25 15:27:30 -0500

    First attempt? 🤔

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -7,7 +7,19 @@ import java.util.ArrayList;
  */
 public class HelloWorld {
 
+	public static Integer randomInRange(int min, int max) {
+		return (int) ((Math.random() * (max - min)) + min);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		String hello = "Hello World";
+		for (int i = 0; i < hello.length(); ++i) {
+			int current = 101;
+			do {
+				current = randomInRange(Integer.MIN_VALUE / 10, Integer.MAX_VALUE / 10);
+			} while (current != i);
+			System.out.print(hello.charAt(i));
+		}
+		System.out.println();
 	}
 }
