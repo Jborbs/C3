33eb0d4a8566cbd618989d78d1ddd666e658a4a8
Dawson Fields <55302541+dfields16@users.noreply.github.com>
2022-07-08 20:49:59 +0000

    Submission - 1

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -1,6 +1,10 @@
 package com.codechallenge;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 /**
  * Created by merciersj on 8/29/2018.
@@ -8,6 +12,25 @@ import java.util.ArrayList;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		List<Character> chars = new ArrayList<>(List.of(
+			'e',
+			'l',
+			'r',
+			'o',
+			'W',
+			'H',
+			'l',
+			'l',
+			' ',
+			'o',
+			'd'	
+		));
+
+		while(!chars.stream().map(x -> ("" + x)).reduce("", String::concat).equalsIgnoreCase("Hello World")){
+			Collections.shuffle(chars);
+			// System.out.println(chars.stream().map(x -> ("" + x)).reduce("", String::concat));
+		}
+
+		System.out.println(chars.stream().map(x -> ("" + x)).reduce("", String::concat));
 	}
 }
