c373ed0e1b81953adc0ad7f2007b4bb4d04bc016
Ronaldo Suarez <ronaldo3055@gmail.com>
2022-07-08 15:56:25 -0500

    gogogog

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -5,9 +5,351 @@ import java.util.ArrayList;
 /**
  * Created by merciersj on 8/29/2018.
  */
+
+interface Letter {
+	char letter = 0;
+}
+
+class uA implements Letter{
+	char letter = 'A';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uB implements Letter{
+	char letter = 'B';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uC implements Letter{
+	char letter = 'C';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uD implements Letter{
+	char letter = 'D';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uE implements Letter{
+	char letter = 'E';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uF implements Letter{
+	char letter = 'F';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uG implements Letter{
+	char letter = 'G';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uH implements Letter{
+	char letter = 'H';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uI implements Letter{
+	char letter = 'I';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uJ implements Letter{
+	char letter = 'J';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uK implements Letter{
+	char letter = 'K';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uL implements Letter{
+	char letter = 'L';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uM implements Letter{
+	char letter = 'M';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uN implements Letter{
+	char letter = 'N';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uO implements Letter{
+	char letter = 'O';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uP implements Letter{
+	char letter = 'P';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uQ implements Letter{
+	char letter = 'Q';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uR implements Letter{
+	char letter = 'R';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uS implements Letter{
+	char letter = 'S';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uT implements Letter{
+	char letter = 'T';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uU implements Letter{
+	char letter = 'U';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uV implements Letter{
+	char letter = 'V';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uW implements Letter{
+	char letter = 'W';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uX implements Letter{
+	char letter = 'X';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class uY implements Letter{
+	char letter = 'Y';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class a implements Letter{
+	char letter = 'a';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class b implements Letter{
+	char letter = 'b';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class c implements Letter{
+	char letter = 'c';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class d implements Letter{
+	char letter = 'd';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class e implements Letter{
+	char letter = 'e';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class f implements Letter{
+	char letter = 'f';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class g implements Letter{
+	char letter = 'g';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class h implements Letter{
+	char letter = 'h';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class i implements Letter{
+	char letter = 'i';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class j implements Letter{
+	char letter = 'j';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class k implements Letter{
+	char letter = 'k';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class l implements Letter{
+	char letter = 'l';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class m implements Letter{
+	char letter = 'm';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class n implements Letter{
+	char letter = 'n';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class o implements Letter{
+	char letter = 'o';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class p implements Letter{
+	char letter = 'p';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class q implements Letter{
+	char letter = 'q';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class r implements Letter{
+	char letter = 'r';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class s implements Letter{
+	char letter = 's';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class t implements Letter{
+	char letter = 't';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+class u implements Letter {
+	char letter = 'u';
+
+	public char getLetter() {
+		return letter;
+	}
+}
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
+
+		uH H = new uH();
+		e e = new e();
+		l l = new l();
+		l l2 = new l();
+		o o = new o();
+		uW W = new uW();
+		o o2 = new o();
+		r r = new r();
+		l l3 = new l();
+		d d = new d();
+
+
 		System.out.println("Hello World");
+
 	}
 }
