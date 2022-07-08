d3d003f3c6c679a6d34d92bb9d470c2ee801dd61
Edwin Hernandez <Edwinhernandez2@my.unt.edu>
2022-07-08 15:54:36 -0500

    inital commit

--- src/main/java/com/codechallenge/HelloWorld.java
+++ src/main/java/com/codechallenge/HelloWorld.java
@@ -1,13 +1,35 @@
 package com.codechallenge;
-
 import java.util.ArrayList;
 
 /**
  * Created by merciersj on 8/29/2018.
  */
-public class HelloWorld {
 
+
+public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+        int valH = (int)(Math.pow(8,2) + ((6) + 2));
+        int valE = (int)(Math.pow(3,4) + ((8) + 12));
+        int valL = (int)(Math.pow(5,3) - ((7) + 10));
+        int valLl = (int)(Math.pow(5,3) - ((7) + 10));
+        int valO = (int)(Math.pow(5,3) - (7 + 7));
+        int valSPACE = (int)( 1 * 29 + 3);
+        int valW = (int)(87);
+        int valOo = (int)(Math.pow(5,3) - (7 + 7));
+        int valR = (int)(Math.pow(5,3) - (6 + 5));
+        int valD = (int)(Math.pow(3,4) + (7 + 12) );
+
+        // (8)^2 + ((6) + 2)
+        System.out.print(((char)valH));
+        System.out.print(((char)valE));
+        System.out.print(((char)valL));
+        System.out.print(((char)valLl));
+        System.out.print(((char)valO));
+        System.out.print(((char)valSPACE));
+        System.out.print(((char)valW));
+        System.out.print(((char)valOo));
+        System.out.print(((char)valR));
+        System.out.print(((char)valLl));
+		System.out.println(((char)valD));
 	}
 }
